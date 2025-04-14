1efeaeb0ec11fadc098662dcfd0b39d59f502d71
Abin Thomas <followgamer102@gmail.com>
2025-04-14 17:59:21 +0530

    metadata updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,16 @@
 import type React from "react"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
+import type { Metadata } from "next";
 
-
+export const metadata: Metadata = {
+  title: "Waffy",
+  description: "Ai innvoations",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -13,7 +21,6 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className="w-screen overflow-hidden overflow-y-scroll">
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-          
           {children}
         </ThemeProvider>
       </body>
