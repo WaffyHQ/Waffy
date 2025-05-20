dab5d0be0c8735c3c85def52dcd4f218878520e2
Aravind Manoj <aravindmng47@gmail.com>
2025-05-20 06:47:54 +0530

    chore: added drizzle supabase

---
+++ db/schema.ts
@@ -0,0 +1,12 @@
+import { pgTable, uuid, text, bigint, date, timestamp } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+    id: uuid().defaultRandom().primaryKey().notNull(),
+    accountId: uuid("account_id").defaultRandom().notNull(),
+    firstName: text("first_name"),
+    lastName: text("last_name"),
+    email: text().notNull(),
+    phoneNo: bigint("phone_no", { mode: "number" }).notNull(),
+    dob: date(),
+    createdAt: timestamp("created_at", { withTimezone: true, mode: 'string' }).defaultNow().notNull(),
+});
