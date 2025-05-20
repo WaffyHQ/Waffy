60826151bb166ba93ed02b7b1fd3723d0c8d2e99
Abin Thomas <followgamer102@gmail.com>
2025-05-20 16:54:09 +0530

    feat: Waitlist mailing system

--- app/_components/Waitlist.tsx
+++ app/_components/Waitlist.tsx
@@ -5,11 +5,10 @@ import React from 'react'
 
 export default function Waitlist({ emailRef }: { emailRef: React.RefObject<HTMLInputElement | null> }) {
   const [email, setEmail] = React.useState<string>("");
-
   return (
     <section className="container mx-auto px-4 sm:px-6 mb-20">
       <div className="flex flex-row gap-3 p-2 mt-4 sm:mt-6 items-center justify-center">
-        <Input
+        <Input                                                                                                               
           placeholder="Enter your email to join the waitlist"
           className="w-full max-w-md px-4 py-2 rounded-lg bg-gray-800 text-white placeholder-gray-400 focus:outline-none focus:ring-2 border-0 focus:ring-green-500"
           type="email"
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
-import type React from "react"
-import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
+import type React from "react";
+import "./globals.css";
+import { ThemeProvider } from "next-themes";  
 import type { Metadata } from "next";
 import Navbar from "./_components/Navbar";
+import { ReactQueryProvider } from "./_providers/ReactQueryProvider"; // <-- import it
+
 export const metadata: Metadata = {
   title: "Waffy",
-  description: "Ai innvoations",
+  description: "AI innovations",
   icons: {
     icon: "https://raw.githubusercontent.com/WaffyHQ/Waffy/refs/heads/main/app/favicon.ico",
     shortcut: "https://raw.githubusercontent.com/WaffyHQ/Waffy/refs/heads/main/app/favicon.ico",
@@ -15,20 +17,22 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="w-screen overflow-hidden overflow-y-scroll">
-      <div className="min-h-screen bg-[#000000] flex flex-col ">
-      <Navbar />
-      <main className="flex-grow px-5" >
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-          {children}
-        </ThemeProvider>
-      </main>
-      </div>
+        <div className="min-h-screen bg-[#000000] flex flex-col">
+          <Navbar />
+          <main className="flex-grow px-5">
+             <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+            <ReactQueryProvider>
+              {children}
+            </ReactQueryProvider>
+            </ThemeProvider>
+          </main>
+        </div>
       </body>
     </html>
-  )
+  );
 }
